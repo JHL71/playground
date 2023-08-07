e93daeb98d607ae14494ef7a18afbe2fdae9b3eb
JHL71 <neko0717@naver.com>
2023-08-07 22:23:07 +0900

    update calendar

--- playground/src/page/calendar.js
+++ playground/src/page/calendar.js
@@ -11,26 +11,22 @@ function Calendar() {
   const [month, setMonth] = useState(date.getMonth());
 
   const makeDays = (date) => {
-    console.log(0, date);
-    console.log('x', date.getDay());
     let firstDay = (7 - (date.getDate() % 7 - 1) + date.getDay()) % 7;
-    console.log('y', firstDay);
+    
     let lenp, lenc
     if (date.getFullYear() % 4 === 0) {
       lenp = ly[(date.getMonth() + 11) % 12];
       lenc = ly[date.getMonth()];
-      // lenn = ly[(date.getMonth() + 1) % 12];
     } else {
       lenp = monthLength[(date.getMonth() + 11) % 12];
       lenc = monthLength[date.getMonth()];
-      // lenn = monthLength[(date.getMonth() + 1) % 12];
     }
     let prev = Array.from({length: firstDay}, (_, i) => lenp - firstDay + i + 1);
     let cur = Array.from({length: lenc}, (_, i) => i + 1);
     let next = Array.from({length: (7 - (firstDay + lenc) % 7) % 7}, (_, i) => i + 1);
     let temp = [...prev, ...cur, ...next];
     let result = [];
-    console.log(date);
+    
     for (let i = 0; i < temp.length / 7; i++) {
       result.push([...temp.slice(i*7, i*7 + 7)]);
     }
@@ -71,7 +67,7 @@ function Calendar() {
   }
 
   useEffect(() => {
-    console.log(1);
+
   })
 
   return (
@@ -85,31 +81,36 @@ function Calendar() {
           </Ul>
         </MonthWrap>
         <Table>
-          <thead>
-            <tr>
-              <th>일</th>
-              <th>월</th>
-              <th>화</th>
-              <th>수</th>
-              <th>목</th>
-              <th>금</th>
-              <th>토</th>
-            </tr>
-          </thead>
+          <Thead>
+            <Tr>
+              <Th>일</Th>
+              <Th>월</Th>
+              <Th>화</Th>
+              <Th>수</Th>
+              <Th>목</Th>
+              <Th>금</Th>
+              <Th>토</Th>
+            </Tr>
+          </Thead>
           <tbody>
             {
               days.map((el, i) => {
                 return (
-                  <tr key={i}>
+                  <Tr key={i}>
                     {el.map((num, j) => {
                       return (
                         Math.abs(i * 7 + j - num) < 10 
-                        ? <Td key={j*10}>{num}</Td>
-                        : <Td className='pon' key={j*10}>{num}</Td>
+                        ? <Td key={j*10}>
+                            <DivDate>
+                              <DivNum check={`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}` === `${year}-${month}-${num}`}>{num}
+                              </DivNum>
+                            </DivDate>
+                          </Td>
+                        : <Td className='pon' key={j*10}><DivDate><DivNum>{num}</DivNum></DivDate></Td>
                       )
                     })
                     }
-                  </tr>
+                  </Tr>
                 )
               })
             }
@@ -125,7 +126,7 @@ const Section = styled.div`
   margin: 0px;
   width: 100%;
   height: 100vh;
-  background-color: lightsalmon;
+  background-color: #ffffff;
 `
 
 const MonthWrap = styled.div`
@@ -160,13 +161,50 @@ const Li = styled.li`
 const Table = styled.table`
   width: 100%;
   height: 80vh;
+  border-collapse: collapse;
+`
+
+const Thead = styled.thead`
+  background-color: #e5e5e5;
+  height: 60px;
+`
+
+const Tr = styled.tr`
+  border-bottom: 1px solid #e5e5e5;
+`
+
+const Th = styled.th`
+  padding: 0 1px 0 0;
+  text-align: center;
 `
 
 const Td = styled.td`
+  font-size: 16px;
   color: black;
   &.pon {
-    color: gray;
+    color: #e5e5e5;
   }
+`
+
+const DivDate = styled.div`
+  min-height: 140px;
+  padding: 10px 0px;
+  &:hover {
+    background-color: #e5e5e5;
+  }
+`
+
+const DivNum = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto 7px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  font-size: 20px;
+  background-color: ${(props) => props.check ? '#FDB5CB' : 'inherit'};
+
 `
 
 export default Calendar;
