571e6c04bbd1f70346eb9ac75a8260eda2cb4b3a
JHL71 <neko0717@naver.com>
2023-10-03 20:39:50 +0900

    infinite slide

--- src/page/slide.js
+++ src/page/slide.js
@@ -6,6 +6,7 @@ import img4 from '../img/img4.jpg';
 import img5 from '../img/img5.jpg';
 import img6 from '../img/img6.jpg';
 import { useState } from 'react';
+import { useRef } from 'react';
 
 const Wrap = styled.div`
   width: 100%;
@@ -22,15 +23,21 @@ const SlideContainer = styled.div`
 `
 
 const SlideWrap = styled.div`
+  z-index: 1;
   width: 600px;
   height: 400px;
   display: flex;
   transition-property: transform;
-  transform: translate3d(${props => (props.$state - 1) * -600}px, 0, 0);
-  transition: all 1s ease-out;
+  transform: translate3d(${props => props.$state * -600}px, 0, 0);
+  transition: transform 1s ease-out;
 `
 
 const PositionBar = styled.div`
+  position: absolute;
+  bottom: 20%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  z-index: 2;
   width: 200px;
   height: 20px;
   border-radius: 10px;
@@ -51,34 +58,65 @@ const Img = styled.img`
   width: 600px;
 `
 
+const Button = styled.button`
+  width: 50px;
+  height: 50px;
+  font-size: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border: none;
+  cursor: pointer;
+  &:hover {
+    color: white;
+  }
+`
+
 const Slide = () => {
   const [select, setSelect] = useState(1);
+  const slideRef = useRef(null);
 
   const onClick = ({ target: { name } }) => {
     if (name === 'left') {
+      if (select === 1) {
+        let original = slideRef.current.syle;
+        slideRef.current.setAttribute('style', `transform: translate3d(-4200px, 0, 0); transition: 0s`);
+        setTimeout(() => {
+          slideRef.current.setAttribute('style', original);
+        });
+      }
       setSelect(prev => prev !== 1 ? prev - 1 : 6);
     }
     if (name === 'right') {
+      if (select === 6) {
+        let original = slideRef.current.syle;
+        slideRef.current.setAttribute('style', `transform: translate3d(0, 0, 0); transition: 0s`);
+        setTimeout(() => {
+          slideRef.current.setAttribute('style', original);
+        });
+      }
       setSelect(prev => prev !== 6 ? prev + 1 : 1);
     }
   }
 
   return (
     <Wrap>
+      <Button name='left' onClick={onClick}>◀</Button>
       <SlideContainer>
-        <SlideWrap $state={select}>
-          <Img src={`${img6}`} alt="img6"/>
-          <Img src={`${img1}`} alt="img1"/>
-          <Img src={`${img2}`} alt="img2"/>
-          <Img src={`${img3}`} alt="img3"/>
-          <Img src={`${img4}`} alt="img4"/>
-          <Img src={`${img5}`} alt="img5"/>
-          <Img src={`${img6}`} alt="img6"/>
-          <Img src={`${img1}`} alt="img1"/>
+        <SlideWrap $state={select} ref={slideRef}>
+          <Img $num={0} $state={select} src={`${img6}`} alt="img6"/>
+          <Img $num={1} $state={select} src={`${img1}`} alt="img1"/>
+          <Img $num={2} $state={select} src={`${img2}`} alt="img2"/>
+          <Img $num={3} $state={select} src={`${img3}`} alt="img3"/>
+          <Img $num={4} $state={select} src={`${img4}`} alt="img4"/>
+          <Img $num={5} $state={select} src={`${img5}`} alt="img5"/>
+          <Img $num={6} $state={select} src={`${img6}`} alt="img6"/>
+          <Img $num={7} $state={select} src={`${img1}`} alt="img1"/>
         </SlideWrap>
       </SlideContainer>
-      <button name='left' onClick={onClick}>◀</button>
-      <button name='right' onClick={onClick}>▶</button>
+      <Button name='right' onClick={onClick}>▶</Button>
       <PositionBar>
         <Circle $num={1} $state={select}></Circle>
         <Circle $num={2} $state={select}></Circle>
